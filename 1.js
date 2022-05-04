749cc50b82055bc12aa74f80d6df20cd3ea8ed6c
cjhkemp <103396649+cjhkemp@users.noreply.github.com>
2022-05-04 15:39:45 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+x=9
+text=prompt("hello, please enter your identity, parent or student")
+if(text=="student"){
+     location.replace("https://cjhkemp.github.io/4.25/detail.html")
+}else if(text=="parent"){
+     location.replace("https://cjhkemp.github.io/4.25/index.html")
+}
